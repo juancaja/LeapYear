0b913c1ad2ca03eeccddc8b526dbf6d0c033cbd4
Juan Antonio Caja García <juanantonio.caja.contractor@bbva.com>
2017-05-11 18:52:54 +0200

    New year 2000 test passing and refactored code

--- java/src/com/minikata/LeapYear.java
+++ java/src/com/minikata/LeapYear.java
@@ -5,14 +5,8 @@ public final class LeapYear {
 	public static boolean isLeapYear(int year) {
 		boolean isLeapYear = false;
 
-		if (year % 4 == 0) { // year is divisible by 4
-			if (year % 100 == 0) { // year is divisible by 100
-				isLeapYear = false;
-			} else {
-				isLeapYear = true;
-			}
-		} else {
-			isLeapYear = false;
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+			isLeapYear = true;
 		}
 
 		return isLeapYear;
