7874134fffaec560adec533650b4daafb175f978
Juan Antonio Caja García <juanantonio.caja.contractor@bbva.com>
2017-05-11 18:25:18 +0200

    First commit with first test failire (TDD)

---
+++ java/src/com/minikata/LeapYear.java
@@ -0,0 +1,9 @@
+package com.minikata;
+
+public final class LeapYear {
+
+	public static boolean isLeapYear(int year) {
+		return false;
+	}
+
+}
---
+++ java/src/com/minikata/Main.java
@@ -0,0 +1,8 @@
+package com.minikata;
+
+public class Main {
+	public static void main(String[] args) {
+
+		System.out.println("Hola Mundo");
+	}
+}
