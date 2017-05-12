b093d4c0f8a294dd120502e5b010116e12815ee7
Juan Antonio Caja García <juanantonio.caja.contractor@bbva.com>
2017-05-12 16:50:20 +0200

    New failing test for year 100

--- javascript/leap-year.js
+++ javascript/leap-year.js
@@ -10,4 +10,12 @@ describe('Leap Year Kata', function(){
 	it('should return true for year 4', function(){
 		assert.equal(true, isLeapYear(4));
 	});
+
+	it('should return false for year 3', function(){
+		assert.equal(false, isLeapYear(3));
+	});
+
+	it('should return false for year 100', function(){
+		assert.equal(false, isLeapYear(100));
+	});
 });
